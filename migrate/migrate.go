0aa0268bf3628086891b2a418ea67c89e5910b98
KN <nk29time@gmail.com>
2025-03-11 12:16:07 +0900

    fix migrate.go

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"go-rest-api/db"
 	"go-rest-api/model"
+	"log"
 )
 
-func main(){
+func main() {
 	dbConn := db.NewDB()
-	defer fmt.Println("Successfully Migrated")
+	if dbConn == nil {
+		log.Panicf("Failed to connect DB :%v", dbConn)
+	}
 	defer db.CloseDB(dbConn)
-	dbConn.AutoMigrate(&model.User{},&model.Task{})
+	err := dbConn.AutoMigrate(&model.User{}, &model.Task{})
+	if err != nil {
+		log.Panicf("Failed to migrate db: %v", err)
+	}
+	fmt.Println("Successfully Migrated")
 }
